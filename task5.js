92cbc7dfab42bd9c94b867a2e02064175e0b0aa1
CHETANA69 <chetanapawar069@gmail.com>
2023-03-17 21:21:11 +0530

    tagname

---
+++ task5.js
@@ -0,0 +1,14 @@
+// GETELEMENTSBYCLASSNAMES //
+// var items = document.getElementsByClassName('list-group-item');
+// for (var i = 0;i < items.length;i++)
+// {
+//     items[i].style.fontWeight = 'bold';
+// }
+
+// GETELEMENTSBYTAGNAME //
+var li = document.getElementsByTagName('li');
+// console.log(li);
+for (var i = 0;i < li.length;i++)
+{
+    li[i].style.fontWeight = 'bold';
+}
