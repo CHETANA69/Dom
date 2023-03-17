2cd4656b23d831fdc2adb2e98ae4903f45f6e0fe
CHETANA69 <chetanapawar069@gmail.com>
2023-03-17 20:07:58 +0530

    task3

---
+++ dom.js
@@ -0,0 +1,18 @@
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 1245;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all[9]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+  
+// GETELEMENTBYID
+var header = document.getElementById('main-header');
+header.style.border ='solid 3px #000';
+var t1 = document.getElementById('item');
+t1.style.fontWeight = "bold"
+t1.style.color= "green";
